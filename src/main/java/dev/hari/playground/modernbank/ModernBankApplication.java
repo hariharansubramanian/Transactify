e59e4b680188ef1495782fe5ec4f45aa40edd287
Hariharan Subramanian <hari@flowinsights.com>
2023-04-07 13:06:39 +0200

    Scaffold SpringBoot application with Maven

---
+++ src/main/java/dev/hari/playground/modernbank/ModernBankApplication.java
@@ -0,0 +1,13 @@
+package dev.hari.playground.modernbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModernBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModernBankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/hari/playground/modernbank/ModernBankApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.hari.playground.modernbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModernBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
